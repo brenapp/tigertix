3545afb1b0065f6e70eb28a936ebf94ad71d68c0
Brendan McGuire <brendan@bren.app>
2022-03-08 21:57:10 -0500

    formatting

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,10 @@
 import "../styles/globals.css";
 import "../styles/index.css";
+
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
