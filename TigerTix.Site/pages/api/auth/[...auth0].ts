74c0e1c9f7d27641d44731d3f4d06f302f838b6a
Brendan McGuire <brendan@bren.app>
2022-03-14 23:35:08 -0400

    Manual override for production apps

--- TigerTix.Site/pages/api/auth/[...auth0].ts
+++ TigerTix.Site/pages/api/auth/[...auth0].ts
@@ -3,5 +3,8 @@ import { handleAuth } from "@auth0/nextjs-auth0";
 // Overwrite base URL for Auth0 with Vercel URL if present (for deployed and preview branches)
 // See https://github.com/auth0/nextjs-auth0/issues/383#issuecomment-1026071966
 process.env.AUTH0_BASE_URL = process.env.AUTH0_BASE_URL || process.env.VERCEL_URL;
+if (process.env.VERCEL_ENV === "production") {
+    process.env.AUTH0_BASE_URL = "https://tigertix.bren.app";
+};
 
 export default handleAuth();
