cba0cce3548be4aceadf21b7f9df8a13fc66ad70
Justin <jlkris007@gmail.com>
2022-04-04 20:27:56 -0400

    Models, DbContext, Migrations

---
+++ TigerTix.Api/TigerTix.Api.Data/Enums/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace TigerTix.Api.Data.Enums; 
+
+public enum Role {
+    
+    User = 1,
+    Crew = 2,
+    Artist = 3
+    
+}
---
+++ TigerTix.Api/TigerTix.Api.Data/Models/ShowAssociate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TigerTix.Api.Data.Enums;
+
+namespace TigerTix.Api.Data.Models; 
+
+[Index(nameof(ShowId))]
+public class ShowAssociate {
+
+    public ulong Id { get; set; }
+    
+    public ulong ShowId { get; set; }
+    public Show Show { get; set; }
+    
+    public ulong UserId { get; set; }
+    public User User { get; set; }
+
+    public Role Role { get; set; } = Role.User;
+
+}
