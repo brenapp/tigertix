01f2a95d7138f352f7fd505553061bce1e3caba3
Brendan McGuire <brendan@bren.app>
2022-04-14 18:45:30 -0400

    Support all mouse events

--- TigerTix.Site/components/button.tsx
+++ TigerTix.Site/components/button.tsx
@@ -3,7 +3,14 @@ import { MouseEventHandler } from "react";
 export interface ButtonProps {
     color: "primary" | "none";
     className?: string;
-    onClick: MouseEventHandler;
+    onClick?: MouseEventHandler;
+    onMouseDown?: MouseEventHandler;
+    onMouseUp?: MouseEventHandler;
+    onMouseEnter?: MouseEventHandler;
+    onMouseLeave?: MouseEventHandler;
+    onTouchStart?: MouseEventHandler;
+    onTouchEnd?: MouseEventHandler;
+    onTouchCancel?: MouseEventHandler;
 };
 
 const Button: React.FC<ButtonProps> = ({ className, children, onClick, color }) => {
