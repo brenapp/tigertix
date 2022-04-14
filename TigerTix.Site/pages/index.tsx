1ceb7c4bc15ba6bdd55145c7d58b08c8a0749f43
Brendan McGuire <brendan@bren.app>
2022-04-14 11:46:23 -0400

    Add some copy to post event link

--- TigerTix.Site/pages/index.tsx
+++ TigerTix.Site/pages/index.tsx
@@ -31,12 +31,15 @@ const Home: NextPage<{ events: Event[] }> = ({ events }) => {
                             />
                         ))}
                     </div>
-                    <Link href="/events/create">
-                        <a className="with-icon group italic text-orange">
-                            Post An Event
-                            <ArrowRightIcon className="h-4 w-4 group-hover:ml-3 ml-2 transition-all" />
-                        </a>
-                    </Link>
+                    <section className="mt-2">
+                        <p>Have an event of your own? Post it to TigerTix to increase its visibility, and unlock event management tools!</p>
+                        <Link href="/events/create">
+                            <a className="with-icon group italic text-orange">
+                                Post An Event
+                                <ArrowRightIcon className="h-4 w-4 group-hover:ml-3 ml-2 transition-all" />
+                            </a>
+                        </Link>
+                    </section>
                 </section>
                 <section className="venues mx-4 mt-12">
                     <h1 className="text-2xl with-icon">
