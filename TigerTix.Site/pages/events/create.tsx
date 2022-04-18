13c3550ac6c18d888ad1cd6fa453d81e0445f9fc
Brendan McGuire <brendan@bren.app>
2022-04-17 21:31:57 -0400

    Paginate better

--- TigerTix.Site/pages/events/create.tsx
+++ TigerTix.Site/pages/events/create.tsx
@@ -86,7 +86,7 @@ const EventCreateForm = () => {
         const raw = convertToRaw(editorState.getCurrentContent());
         setBlock(blockIndex, {
             description: JSON.stringify(raw),
-        })
+        });
         sessionStorage.setItem("event", JSON.stringify(event));
     }, [editorState]);
 
@@ -292,82 +292,75 @@ const EventCreateForm = () => {
     );
     const EventPreview = () => (
         <section className="preview lg:col-span-1 col-span-2">
-                <section className="grid grid-cols-1">
-                    <section className="image relative mx-auto w-full h-full rounded-md">
-                        <Image
-                            alt={event.description}
-                            src={event.image_hero}
-                            height={500}
-                            width={1500}
-                            className="rounded-md"
-                        />
-                        <nav className="quick-actions absolute top-4 right-4 bg-white rounded-md shadow-lg">
-                            <Button
-                                color="none"
-                                onClick={() => {}}
-                                className="flex items-center p-2"
-                            >
-                                <BeakerIcon className="h-5 w-5 text-gray-700 mr-2" />
-                                Event Preview
-                            </Button>
-                        </nav>
-                    </section>
-                    <section className="info bg-white rounded-md border-2 p-4 md:m-0 m-2">
-                        <h2 className="text-2xl font-bold text-gray-700">
-                            {event.title}
-                        </h2>
-                        <p className="pt-2">{event.description}</p>
-                        <div className="flex mt-4 mx-1">
-                            <CalendarIcon className="h-6 w-6" />
-                            <span className="pl-2">
-                                <p>{dateString}</p>
-                            </span>
-                        </div>
-                        <div className="flex mt-4 mx-1 items-center">
-                            <LocationMarkerIcon className="h-6 w-6" />
-                            <span className="pl-2">
-                                <p className="leading-tight italic">
-                                    {event.venue_description}
-                                </p>
-                                <p className="leading-tight">
-                                    {event.venue_address}
-                                </p>
-                            </span>
-                        </div>
-                        <div className="flex mt-4 mx-1 items-center">
-                            <GlobeAltIcon className="h-6 w-6" />
-                            <span className="pl-2">
-                                <p>{event.capacity_remaining} tickets remain</p>
-                            </span>
-                        </div>
-                        <div className="flex mt-4 mx-1 items-center">
-                            <CurrencyDollarIcon className="h-6 w-6" />
-                            <span className="pl-2">
-                                <p>
-                                    By clicking delete below, you will{" "}
-                                    <span className="font-bold">
-                                        permanently
-                                    </span>{" "}
-                                    remove all of the tabs contents. This action
-                                    cannot be undone.
-                                </p>
-                            </span>
-                        </div>
-                        <div className="w-full mt-4 mx-2">
-                            <Button
-                                color="primary"
-                                className="p-2 px-6 mt-2"
-                                onClick={() => {}}
-                            >
-                                Register
-                            </Button>
-                        </div>
-                    </section>
+            <section className="grid grid-cols-1">
+                <section className="image relative mx-auto w-full h-full rounded-md">
+                    <Image
+                        alt={event.description}
+                        src={event.image_hero}
+                        height={500}
+                        width={1500}
+                        className="rounded-md"
+                    />
+                    <nav className="quick-actions absolute top-4 right-4 bg-white rounded-md shadow-lg">
+                        <Button
+                            color="none"
+                            onClick={() => {}}
+                            className="flex items-center p-2"
+                        >
+                            <BeakerIcon className="h-5 w-5 text-gray-700 mr-2" />
+                            Event Preview
+                        </Button>
+                    </nav>
+                </section>
+                <section className="info bg-white rounded-md border-2 p-4 md:m-0 m-2">
+                    <h2 className="text-2xl font-bold text-gray-700">
+                        {event.title}
+                    </h2>
+                    <p className="pt-2">{event.description}</p>
+                    <div className="flex mt-4 mx-1">
+                        <CalendarIcon className="h-6 w-6" />
+                        <span className="pl-2">
+                            <p>{dateString}</p>
+                        </span>
+                    </div>
+                    <div className="flex mt-4 mx-1 items-center">
+                        <LocationMarkerIcon className="h-6 w-6" />
+                        <span className="pl-2">
+                            <p className="leading-tight italic">
+                                {event.venue_description}
+                            </p>
+                            <p className="leading-tight">
+                                {event.venue_address}
+                            </p>
+                        </span>
+                    </div>
+                    <div className="flex mt-4 mx-1 items-center">
+                        <GlobeAltIcon className="h-6 w-6" />
+                        <span className="pl-2">
+                            <p>{event.capacity_remaining} tickets remain</p>
+                        </span>
+                    </div>
+                    <div className="flex mt-4 mx-1 items-center">
+                        <CurrencyDollarIcon className="h-6 w-6" />
+                        <span className="pl-2">
+                            ${event.registration_price.toFixed(2)}/ticket
+                        </span>
+                    </div>
+                    <div className="w-full mt-4 mx-2">
+                        <Button
+                            color="primary"
+                            className="p-2 px-6 mt-2"
+                            onClick={() => {}}
+                        >
+                            Register
+                        </Button>
+                    </div>
                 </section>
             </section>
-    )
+        </section>
+    );
     const ContentBlockForm = () => (
-        <section className="bg-white rounded-md border-2 px-4 py-2 col-span-2">
+        <section className="bg-white rounded-md border-2 px-4 py-2 col-span-3 md:col-span-2">
             <h2 className="text-orange text-lg italic">Content Blocks</h2>
             <p>
                 Use content blocks to add more details about your event,
@@ -408,12 +401,12 @@ const EventCreateForm = () => {
                         <p>Tab Name</p>
                         <input
                             type="text"
-                            value={event.blocks[blockIndex].title}
-                            onChange={(e) =>
+                            defaultValue={event.blocks[blockIndex].title}
+                            onBlur={(e) => {
                                 setBlock(blockIndex, {
                                     title: e.target.value,
-                                })
-                            }
+                                });
+                            }}
                             className="w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border-2 border-solid rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange hover:border-gray-200 focus:outline-none"
                         />
                     </label>
@@ -452,22 +445,78 @@ const EventCreateForm = () => {
             )}
         </section>
     );
+    const ContentBlockPreview = () => {
+        return (
+            <section className="bg-white rounded-md border-2 col-span-2">
+                <nav className="flex border-b-2 p-2">
+                    {event.blocks.map((event, i) => (
+                        <a
+                            href="#"
+                            key={event.title    }
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setBlockIndex(i);
+                            }}
+                            className={
+                                "text-orange px-4 py-2 rounded-md " +
+                                (blockIndex == i ? "text-white bg-orange" : "")
+                            }
+                        >
+                            {event.title}
+                        </a>
+                    ))}
+                </nav>
+                <div className="p-4">
+                    <ContentBlockDisplay state={editorState} />
+                </div>
+            </section>
+        );
+    };
 
-    const pages = [
-        <>
-            <BasicDetailsForm />
-            <EventPreview />
-        </>,
-        <ContentBlockForm />
+    const pages: [string, JSX.Element][] = [
+        [
+            "Basic Details",
+            <>
+                <BasicDetailsForm />
+                <EventPreview />
+            </>,
+        ],
+        ["Content Blocks", <ContentBlockForm />],
+        [
+            "Review",
+            <>
+                <EventPreview />
+                <ContentBlockPreview />
+                <div className="flex col-span-3 justify-center mt-8">
+                    <Button color="primary" className="p-2 px-6">Publish Event!</Button>
+                </div>
+            </>,
+        ],
     ];
     const [page, setPage] = useState(0);
 
     const dateString = new Date(event.start).toLocaleString();
     return (
-        <section className="create grid lg:grid-cols-3 gap-2 mt-4">
-            <BasicDetailsForm />
-            <EventPreview />
-            <ContentBlockForm />
+        <section className="create grid lg:grid-cols-3 gap-2">
+            <nav className="col-span-3 bg-white h-16 border-2 rounded-md flex justify-center items-center">
+                {pages.map(([title], i) => (
+                    <a
+                        className={
+                            "mx-4 text-orange " +
+                            (i == page ? "font-bold underline" : "")
+                        }
+                        href="#"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setPage(i);
+                        }}
+                        key={title}
+                    >
+                        {title}
+                    </a>
+                ))}
+            </nav>
+            {pages[page][1]}
         </section>
     );
 };
